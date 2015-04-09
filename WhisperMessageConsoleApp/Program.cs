2855b94bffd85f884173f5f5f158c0de3443c3a3
Sing Pang <sing_pang@hotmail.com>
2015-04-09 11:42:03 +0200

    Implemented the console app.
    Code is refactored and we have a first working version
    of our app

--- WhisperMessageConsoleApp/Program.cs
+++ WhisperMessageConsoleApp/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WhisperMessage.Test;
+using WhisperMessage;
 
 namespace WhisperMessageConsoleApp
 {
@@ -10,6 +12,22 @@ namespace WhisperMessageConsoleApp
     {
         static void Main(string[] args)
         {
+            try
+            {
+                IWhisperString whisperStr = new WhisperString();
+                Console.WriteLine("Enter a message: ");
+                var message = Console.ReadLine();
+                var result = whisperStr.ManipulateMessage(message);
+                Console.WriteLine("Entered message: {0} \n Manipulated message: {1}", message, result);
+            }
+
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }

27bb7ea3a9b5c7f21e83a458fd4d279171e7eca9
Sing Pang <sing_pang@hotmail.com>
2015-04-09 10:36:02 +0200

    Added two main tests for first module

---
+++ WhisperMessage.Test/WhisperStringTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WhisperMessage.Test
+{
+    [TestClass]
+    public class WhisperStringTests
+    {
+        private IWhisperString _whisperString = new WhisperString();
+
+        [TestMethod]
+        public void ManipulateMessageWithVowels()
+        {
+            
+            //Arrange
+            string value = "Hej! Detta är ett meddelande.";
+            string expected = "Hij! Ditti ir itt middilindi.";
+
+            //Act
+            string actual = _whisperString.ManipulateMessage(value);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+
+        }
+
+        [TestMethod]
+        public void ManipulateMessageWithoutVowels()
+        {
+
+            //Arrange
+            string value = "ghfklj";
+            string expected = "ghfklj";
+
+            //Act
+            string actual = _whisperString.ManipulateMessage(value);
+
+            //ASsert
+            Assert.AreEqual(expected, actual);
+
+
+
+
+        }
+    }
+}
